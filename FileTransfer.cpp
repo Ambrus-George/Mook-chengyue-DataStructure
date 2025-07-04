d451efde33249bf0d6caba1b22aa22e956747983
Ambrus-George <115487966+Ambrus-George@users.noreply.github.com>
2025-07-04 09:23:56 +0800

    Update FileTransfer.cpp

--- FileTransfer.cpp
+++ FileTransfer.cpp
@@ -1,104 +1,124 @@
 #include <iostream>
-#include <vector>
+#include <cstring>
 
-using namespace std;
+#define MAXSIZE 10000
 
 typedef int ElementType;
 
-void Input_connection(vector<ElementType>& D);
-void Check_connection(vector<ElementType>& D);
-void Check_network(vector<ElementType>& D);
-int FindRoot(vector<ElementType>& D, ElementType tmp);
-void UnionTrees(vector<ElementType>& D, int r1, int r2);
+class NetWork 
+{
+public :
+	NetWork(int num) : N(num)
+	{
+		memset(Data, -1, sizeof(Data));
+	}
+	~NetWork() {}
+
+	void checkTransfer(ElementType a, ElementType b)
+	{
+		ElementType root1 = findRoot(a);
+		ElementType root2 = findRoot(b);
+
+		bool flag = (root1 == root2);
+
+		if (flag)
+		{
+			std::cout << "yes" << std::endl;
+		}
+		else {
+			std::cout << "no" << std::endl;
+		}
+	}
+
+	int numOfComponents()
+	{
+		int countNum = 0;
+
+		for (int i = 0; i < N; i++)
+		{
+			if (Data[i+1] < 0)
+			{
+				countNum++;
+			}
+		}
+
+		return countNum;
+	}
+
+	void inputtingConnection(ElementType a, ElementType b)
+	{
+		ElementType root1 = findRoot(a);
+		ElementType root2 = findRoot(b);
+
+		if (Data[root1] < Data[root2])
+		{
+			Data[root1] += Data[root2];
+			Data[root2] = root1;
+		}
+		else {
+			Data[root2] += Data[root1];
+			Data[root1] = root2;
+		}
+	}
+
+	ElementType findRoot(int a)
+	{
+		int i = a;
+		while (Data[i] > 0)
+		{
+			i = Data[i];
+		}
+
+		return i;
+	}
+
+private:
+	ElementType Data[MAXSIZE + 1];
+	int N;
+};
 
 int main()
 {
-    int N = 0;
-    char w;
+	int N = 0;
+	std::cin >> N;
+	NetWork netw(N);
+	bool whilebool = true;
 
-    cin >> N;
-    
-    //initialize the Group which save data
-    vector<ElementType> Data(N+1, -1);
+	while (whilebool)
+	{
+		char ch = ' ';
+		ElementType a = 0;
+		ElementType b = 0;
+		std::cin >> ch;
+		int numC = 0;
 
-    //read the input until w == 'S'
-    do
-    {
-        cin >> w;
-        /* check the w */
-        switch (w){
-            case 'I': Input_connection(Data);   break;      
-            case 'C': Check_connection(Data);   break;      
-            case 'S': Check_network(Data);      break;
-        }
+		switch (ch)
+		{
+		case 'C':
+			std::cin >> a >> b;
+			netw.checkTransfer(a, b);
+			continue;
 
-    } while (w != 'S');
-    
+		case 'I':
+			std::cin >> a >> b;
+			netw.inputtingConnection(a, b);
+			continue;
 
-    return 0;
-}
+		case 'S':
+			numC = netw.numOfComponents();
+			if (numC == 1)
+			{
+				std::cout << "The network is connected." << std::endl;
+			}
+			else {
+				std::cout << "There are " << numC << " components." << std::endl;
+			}
+			whilebool = false;
+			break;
+		default:
+			break;
+		}
+	}
 
-void Input_connection(vector<ElementType>& D){
-    //read two elements and build the connection
-    ElementType e1, e2;
-    cin >> e1 >> e2;
-
-    //Find two roots
-    int root1, root2;
-    root1 = FindRoot(D, e1);
-    root2 = FindRoot(D, e2);
-
-    //union two trees
-    UnionTrees(D, root1, root2);
-}
-
-void Check_connection(vector<ElementType>& D){
-    ElementType e1, e2;
-    cin >> e1 >> e2;
-
-    int root1 = FindRoot(D, e1);
-    int root2 = FindRoot(D, e2);
-
-    if (root1 == root2)     cout << "yes" << endl;
-    else                    cout << "no"  << endl;
-    
-}
-
-void Check_network(vector<ElementType>& D){
-    int length = D.size();
-
-    int count = 0;
-    for (int i = 1; i < length; i++)
-    {
-        if (D[i] < 0)   count++;
-    }
-
-    if (count == 1){
-        cout << "The network is connected." << endl;    
-        return;
-    }    
-
-    cout << "There are " << count << " components.";
-}
-
-int FindRoot(vector<ElementType>& D, ElementType tmp){
-    if (D[tmp] < 0)
-        return tmp;
-    else
-        return D[tmp] = FindRoot(D, D[tmp]);
-
-}
-
-void UnionTrees(vector<ElementType>& D, int r1, int r2){
-    if (D[r1] < D[r2])
-    {
-        /* Tree 1 is the bigger tree */
-        D[r1] += D[r2];
-        D[r2] = r1;
-    }else{
-        /* Tree 2 is the bigger tree */
-        D[r2] += D[r1];
-        D[r1] = r2;
-    }
-    
+	return 0;
 }
