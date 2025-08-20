8023ce236ae9822572edfd82fe57a5c707fbf626
Ambrus-George <115487966+Ambrus-George@users.noreply.github.com>
2025-08-20 13:23:13 +0800

    Add files via upload

---
+++ CodeFiles/Sort4.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+
+typedef int ElementType;
+typedef int IndexType;
+
+int main()
+{
+	IndexType N;
+	std::cin >> N;
+	std::vector<ElementType> Data(51, 0);
+
+	for (int i = 0; i < N; i++)
+	{
+		ElementType item = 0;
+		std::cin >> item;
+		Data.at(item)++;
+	}
+
+	for (int i = 0; i < Data.size(); i++)
+	{
+		if (Data.at(i))
+		{
+			std::cout << i << ":" << Data.at(i) << std::endl;
+		}
+	}
+
+
+
+	return 0;
+}
